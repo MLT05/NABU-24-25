dbf439e3aa1239d271416ed36833ed6787078ce1
CarlT09 <carlosteixeira05@ua.pt>
2025-05-21 15:03:36 +0100

    ultimo update - Carlos e CICI 2 imganes: logo e shopping cart

--- NABU_base/Componentes/cp_header.php
+++ NABU_base/Componentes/cp_header.php
@@ -1,7 +1,10 @@
-<header class="cabecalho fixed-top">
-    <div>
-        <div>
-            <img src="../Imagens/img_cp/logo_nabu.svg
+<header class="cabecalho fixed-top container">
+    <div class="row py-3">
+        <div class="col">
+            <img src="../Imagens/img_cp/logo_nabu.svg">
+        </div>
+        <div class="col">
+            <img src="../Imagens/img_cp/shopping_cart_nabu.svg">
         </div>
     </div>
 </header>
