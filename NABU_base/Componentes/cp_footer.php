9476ccdb7749d28579992fec3ebd4a3a6b3b0c07
seabruh <francisco.seabra@ua.pt>
2025-05-22 14:42:19 +0100

    oizinho

--- NABU_base/Componentes/cp_footer.php
+++ NABU_base/Componentes/cp_footer.php
@@ -1,47 +1,24 @@
-<footer class="fixed-bottom verde_bg container-fluid">
-
-    <div id="botoes_footer" class="row ">
-
-        <a class="col-auto" href="../paginas/index.php">
-            <div  class="align-content-center">
-            <img class="icon_footer" src="../Imagens/icons/home_24dp_FFFFFF_FILL0_wght400_GRAD0_opsz24.svg">
-            <p>Íncio</p>
-        </div>
+<footer class="footer-menu fixed-bottom verde_bg">
+    <div class="d-flex w-100 h-100">
+        <a class="menu-item flex-fill text-center text-white text-decoration-none" href="../paginas/index.php">
+            <img src="../Imagens/icons/home_24dp_FFFFFF_FILL0_wght400_GRAD0_opsz24.svg" alt="Início" class="menu-icon">
+            <p class="menu-label">Início</p>
         </a>
-
-        <a class="col-auto" href="../paginas/pesquisa.php">
-            <div class="align-content-center">
-            <img class="icon_footer" src="../Imagens/icons/search_24dp_FFFFFF_FILL0_wght400_GRAD0_opsz24.svg">
-            <p>Pesquisa</p>
-        </div>
+        <a class="menu-item flex-fill text-center text-white text-decoration-none" href="../paginas/pesquisa.php">
+            <img src="../Imagens/icons/search_24dp_FFFFFF_FILL0_wght400_GRAD0_opsz24.svg" alt="Pesquisa" class="menu-icon ">
+            <p class="menu-label">Pesquisa</p>
         </a>
-
-        <a class="col-auto" href="../paginas/venda.php">
-        <div class="align-content-center">
-            <img class="icon_footer" src="../Imagens/icons/add_circle_24dp_FFFFFF_FILL0_wght400_GRAD0_opsz24.svg">
-            <p>Vender</p>
-        </div>
-
+        <a class="menu-item flex-fill text-center text-white text-decoration-none" href="../paginas/venda.php">
+            <img src="../Imagens/icons/add_circle_24dp_FFFFFF_FILL0_wght400_GRAD0_opsz24.svg" alt="Vender" class="menu-icon">
+            <p class="menu-label">Vender</p>
         </a>
-
-        <a class="col-auto" href="../paginas/mensagens.php">
-        <div class="align-content-center">
-            <img class="icon_footer" src="../Imagens/icons/chat_24dp_FFFFFF_FILL0_wght400_GRAD0_opsz24.svg">
-            <p>Mensagens</p>
-        </div>
+        <a class="menu-item flex-fill text-center text-white text-decoration-none" href="../paginas/mensagens.php">
+            <img src="../Imagens/icons/chat_24dp_FFFFFF_FILL0_wght400_GRAD0_opsz24.svg" alt="Mensagens" class="menu-icon">
+            <p class="menu-label">Mensagens</p>
         </a>
-
-        <a class="col-auto" href="../paginas/perfil.php">
-        <div class="align-content-center">
-            <img class="icon_footer" src="../Imagens/icons/person_24dp_FFFFFF_FILL0_wght400_GRAD0_opsz24.svg">
-            <p>Vender</p>
-        </div>
+        <a class="menu-item flex-fill text-center text-white text-decoration-none" href="../paginas/perfil.php">
+            <img src="../Imagens/icons/person_24dp_FFFFFF_FILL0_wght400_GRAD0_opsz24.svg" alt="Perfil" class="menu-icon">
+            <p class="menu-label">Perfil</p>
         </a>
-
     </div>
-
 </footer>
-
-</body>
-
-
