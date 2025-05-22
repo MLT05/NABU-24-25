5defd75be5b87a22c30bd117d70bab78fd5ec28c
Lucas <noirlove13@gmail.com>
2025-05-22 11:00:44 +0100

    Rearranged code

--- NABU_base/Componentes/CP_Levantamentos.php
+++ NABU_base/Componentes/CP_Levantamentos.php
@@ -1,31 +1,9 @@
-// Nav bar
-<!DOCTYPE html>
-<html lang="pt-pt">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>NABU- Levantamentos</title>
-        <!-- Bootstrap -->
-        <link
-        href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css"
-        rel="stylesheet"
-        integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT"
-        crossorigin="anonymous"
-      />
-      <!-- Tipo de Letra -->
-      <link rel="stylesheet" href="" />
-      <!-- Favicon -->
-      <link
-        rel="shortcut icon"
-        href="/LOGO_ICO/favicon.ico"
-        type="image/x-icon"
-      />
-      <!-- CSS -->
-      <link rel="stylesheet" href="/Styles/styles.css" />
-</head>
+//ligação á base de dados
+ <?php 
+ 
+ ?>
 
-<body>
     <div class="container">
         <div class="row">
             <div class="col-12">
@@ -34,12 +12,12 @@
 
             <div class="col-10">
                 <img src="/NABU_base/Imagens/USER_ex.jpeg" alt="Produto" class>
-                <h2 class="text-center">Levante a sua emcomenda {$num_ecomend} </h2>
-                <p>A morada ou ponto de entrega é o seguinte: {$local}</p>
+                <span class="badge text-bg-secondary">!</span>
+                <h2 class="text-center">Levante a sua emcomenda <?= {$num_ecomend}?> </h2>
+                <p>A morada ou ponto de entrega é o seguinte: <?= {$local}?> </p>
             </div>
         </div>
         
     </div>
-</body>
 
-</html>
+
