e279a2b959ecc480b538de882ec72efe16e2d31f
seabruh <francisco.seabra@ua.pt>
2025-05-22 15:16:34 +0100

    Changes by Carlao

--- NABU_base/Componentes/cp_header.php
+++ NABU_base/Componentes/cp_header.php
@@ -1,10 +1,10 @@
 <header class="cabecalho fixed-top container-fluid">
-    <div class="row py-3">
-        <div class="col">
-            <img src="../Imagens/img_cp/logo_nabu.svg">
+    <div class="row py-3 align-items-center justify-content-between">
+        <div class="col-auto">
+            <img src="../Imagens/img_cp/logo_nabu.svg" alt="Logo" class="logo-img">
         </div>
-        <div class="col">
-            <img src="../Imagens/img_cp/shopping_cart_nabu.svg">
+        <div class="col-auto">
+            <img src="../Imagens/img_cp/shopping_cart_nabu.svg" alt="Carrinho" class="carrinho-img">
         </div>
     </div>
 </header>
