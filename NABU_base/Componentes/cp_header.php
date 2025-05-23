94c09afee9117efb0d4e53504ce71ab616e543ae
seabruh <francisco.seabra@ua.pt>
2025-05-23 13:58:50 +0100

    ola

--- NABU_base/Componentes/cp_header.php
+++ NABU_base/Componentes/cp_header.php
@@ -1,10 +1,10 @@
 <header class="cabecalho fixed-top container-fluid">
     <div class="row py-3 align-items-center justify-content-between">
-        <div class="col-auto">
+        <a href="../Paginas/index.php" class="col-auto">
             <img src="../Imagens/img_cp/logo_nabu.svg" alt="Logo" class="logo-img">
-        </div>
-        <div class="col-auto">
+        </a>
+        <a href="../Paginas/carrinho.php" class="col-auto">
             <img src="../Imagens/img_cp/shopping_cart_nabu.svg" alt="Carrinho" class="carrinho-img">
-        </div>
+        </a>
     </div>
 </header>
---
+++ NABU_base/Componentes/cp_teste_seabra.php
@@ -0,0 +1,14 @@
+<div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+            <img src="../Imagens/produtos/alface.jpg" class="d-block w-100" alt="...">
+        </div>
+        <div class="carousel-item">
+            <img src="../Imagens/produtos/ovos.jpg" class="d-block w-100" alt="...">
+        </div>
+        <div class="carousel-item">
+            <img src="../Imagens/produtos/alface.jpg" class="d-block w-100" alt="...">
+        </div>
+    </div>
+</div>
+
