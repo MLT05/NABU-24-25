8823b983d6a93ae365a571be6602c63d21f746ab
seabruh <francisco.seabra@ua.pt>
2025-05-23 16:49:34 +0100

    seabraaa

---
+++ NABU_base/Paginas/mapa.php
@@ -0,0 +1,7 @@
+<?php include_once "../Componentes/cp_head.php" ?>
+
+<?php include_once "../Componentes/cp_header.php" ?>
+
+<?php include_once "../Componentes/cp_mapa.php" ?>
+
+<?php include_once "../Componentes/cp_footer.php" ?>
