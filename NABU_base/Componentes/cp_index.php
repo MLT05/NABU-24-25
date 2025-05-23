2adb77089987ad92feaf2ac362b4205f1ec871c9
seabruh <francisco.seabra@ua.pt>
2025-05-23 14:31:44 +0100

    ola

--- NABU_base/Componentes/cp_index.php
+++ NABU_base/Componentes/cp_index.php
@@ -4,33 +4,23 @@ include_once ("cp_intro_index.php");
 
 <main class="body_index">
 
-    <div class="cards_homepage card-body">
+    <div class="cards_homepage card-body rounded-4 shadow-sm" style="background-color: #dce7db; position: relative;">
+
+        <!-- Imagem -->
         <div class="imagem_card_homepage">
-            <img class="img_hp_card" src="../Imagens/produtos/ovos.jpg">
+            <img class="img_hp_card" src="../Imagens/produtos/alface.jpg" alt="Alface">
         </div>
 
+        <!-- Texto -->
         <div class="p-3">
-            <h2>
-                Ovos (unidade)
-            </h2>
-
-
-            <p>
-                Ermelinda almeida -
-            </p>
-
-            <p>
-                1.2 km
-            </p>
-
-            <h3 class="align-content-end">
-               2,49 €
-            </h3>
+            <h2 class="verde fw-bold mb-1" >Alface/kg</h2>
+            <p class="text-muted mb-2" style="font-size: 1rem;">Alfredo Rocha - Quinta Coelho</p>
+            <hr>
+            <div class="d-flex justify-content-end">
+                <h3 class="verde_escuro fw-semibold mb-0">2,49 €</h3>
+            </div>
         </div>
     </div>
-
-
-
 
 
 
