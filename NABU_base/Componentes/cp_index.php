4e9e8d75276ba089f2405cbe49abd40adf120d3a
seabruh <francisco.seabra@ua.pt>
2025-05-23 15:31:55 +0100

    ola

--- NABU_base/Componentes/cp_index.php
+++ NABU_base/Componentes/cp_index.php
@@ -4,27 +4,75 @@ include_once ("cp_intro_index.php");
 
 <main class="body_index">
 
-    <div class="cards_homepage card-body rounded-4 shadow-sm" style="background-color: #dce7db; position: relative;">
+    <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <div class="cards_homepage card-body rounded-4 shadow-sm" style="background-color: #dce7db; position: relative;">
 
-        <!-- Imagem -->
-        <div class="imagem_card_homepage">
-            <img class="img_hp_card" src="../Imagens/produtos/alface.jpg" alt="Alface">
-        </div>
+                    <!-- Imagem -->
+                    <div class="imagem_card_homepage">
+                        <img class="img_hp_card" src="../Imagens/produtos/alface.jpg" alt="Alface">
+                    </div>
 
-        <!-- Texto -->
-        <div class="p-3">
-            <h2 class="verde fw-bold mb-1" >Alface/kg</h2>
-            <p class="text-muted mb-2" style="font-size: 1rem;">Alfredo Rocha - Quinta Coelho</p>
-            <hr>
-            <div class="d-flex justify-content-end">
-                <h3 class="verde_escuro fw-semibold mb-0">2,49 €</h3>
+                    <!-- Texto -->
+                    <div class="p-3">
+                        <h2 class="verde fw-bold mb-1" >Alface/kg</h2>
+                        <p class="text-muted mb-2" style="font-size: 1rem;">Alfredo Rocha - Quinta Coelho</p>
+                        <hr>
+                        <div class="d-flex justify-content-end">
+                            <h3 class="verde_escuro fw-semibold mb-0">2,49 €</h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <div class="cards_homepage card-body rounded-4 shadow-sm" style="background-color: #dce7db; position: relative;">
+
+                    <!-- Imagem -->
+                    <div class="imagem_card_homepage">
+                        <img class="img_hp_card" src="../Imagens/produtos/alface.jpg" alt="Alface">
+                    </div>
+
+                    <!-- Texto -->
+                    <div class="p-3">
+                        <h2 class="verde fw-bold mb-1" >Alface/kg</h2>
+                        <p class="text-muted mb-2" style="font-size: 1rem;">Alfredo Rocha - Quinta Coelho</p>
+                        <hr>
+                        <div class="d-flex justify-content-end">
+                            <h3 class="verde_escuro fw-semibold mb-0">2,49 €</h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <div class="cards_homepage card-body rounded-4 shadow-sm" style="background-color: #dce7db; position: relative;">
+
+                    <!-- Imagem -->
+                    <div class="imagem_card_homepage">
+                        <img class="img_hp_card" src="../Imagens/produtos/alface.jpg" alt="Alface">
+                    </div>
+
+                    <!-- Texto -->
+                    <div class="p-3">
+                        <h2 class="verde fw-bold mb-1" >Alface/kg</h2>
+                        <p class="text-muted mb-2" style="font-size: 1rem;">Alfredo Rocha - Quinta Coelho</p>
+                        <hr>
+                        <div class="d-flex justify-content-end">
+                            <h3 class="verde_escuro fw-semibold mb-0">2,49 €</h3>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
+        <button class="carousel-control-prev" type="button"  data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
     </div>
-
-
-
-
 
 </main>
 
