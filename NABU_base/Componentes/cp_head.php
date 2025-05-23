b0243ce6104b145d45a6027e7c8db7de755fbc36
MLT05 <marianatavares18@ua.pt>
2025-05-23 17:19:58 +0100

    MARIANA

--- NABU_base/Componentes/cp_head.php
+++ NABU_base/Componentes/cp_head.php
@@ -1,26 +1,30 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-        <meta name="description" content="Web-app para compra venda e troca de produtos locais alimentares." />
-        <meta name="author" content="G13-bdtss" />
-        <title>Nabu</title>
-        <link rel="icon" type="image/x-icon" href="imgs/favicon.ico" />
-        <!-- Font Awesome icons (free version)-->
-        <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
-        <!-- Google fonts-->
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet">
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Jost:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="Web-app para compra venda e troca de produtos locais alimentares." />
+    <meta name="author" content="G13-bdtss" />
+    <title>Nabu</title>
+    <link rel="icon" type="image/x-icon" href="imgs/favicon.ico" />
 
+    <!-- Font Awesome icons (free version)-->
+    <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
 
-        <!-- Core theme CSS (includes Bootstrap)-->
+    <!-- Google fonts-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
 
-        <link href="../CSS/meus_estilos.css" rel="stylesheet" />
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Correção da URL da fonte Jost -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Jost:wght@100;300;400;500;700;800;900&display=swap" rel="stylesheet">
 
-    </head>
-    <body class="corpo container">
+    <!-- Core theme CSS (includes Bootstrap)-->
+    <link href="../CSS/meus_estilos.css" rel="stylesheet" />
+
+</head>
+<body class="corpo container">
+<!-- O conteúdo da página vai aqui -->
+</body>
+</html>
