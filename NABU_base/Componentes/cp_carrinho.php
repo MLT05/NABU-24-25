f3700341f70b746c6fefb2bc293a061f8e6da4c7
MariaLeonorFrias <marialeonorfrias@ua.pt>
2025-05-24 11:23:00 +0100

    Merge remote-tracking branch 'origin/marileide' into marileide

--- NABU_base/Componentes/cp_carrinho.php
+++ NABU_base/Componentes/cp_carrinho.php
@@ -32,7 +32,7 @@ include_once ("cp_intro_carrinho.php");
         <div class="card mb-3 cards_homepage">
             <div class="row g-0 align-items-center">
                 <div class="col-4">
-                    <img src="produtos/tomates.svg" class="img-fluid imagem_card_homepage rounded-start" alt="Tomates">
+                    <img src="imagens/produtos/tomates.svg" class="img-fluid imagem_card_homepage rounded-start" alt="Tomates">
                 </div>
                 <div class="col-7">
                     <div class="card-body py-2">
@@ -41,7 +41,7 @@ include_once ("cp_intro_carrinho.php");
                     </div>
                 </div>
                 <div class="col-1 text-end pe-2">
-                    <img src="img_cp/close_24dp_004D40_FILL0_wght400_GRAD0_opsz24.svg" alt="Remover" class="icone-x">
+                    <img src="imagens/img_cp/close_24dp_004D40_FILL0_wght400_GRAD0_opsz24.svg" alt="Remover" class="icone-x">
 
                 </div>
             </div>
@@ -51,7 +51,7 @@ include_once ("cp_intro_carrinho.php");
         <div class="card mb-3 cards_homepage">
             <div class="row g-0 align-items-center">
                 <div class="col-4">
-                    <img src="produtos/tomates.svg" class="img-fluid imagem_card_homepage rounded-start" alt="Tomates">
+                    <img src="imagens/produtos/tomates.svg" class="img-fluid imagem_card_homepage rounded-start" alt="Tomates">
                 </div>
                 <div class="col-7">
                     <div class="card-body py-2">
@@ -60,7 +60,7 @@ include_once ("cp_intro_carrinho.php");
                     </div>
                 </div>
                 <div class="col-1 text-end pe-2">
-                    <img src="img_cp/close_24dp_004D40_FILL0_wght400_GRAD0_opsz24.svg" alt="Remover" class="icone-x">
+                    <img src="imagens/img_cp/close_24dp_004D40_FILL0_wght400_GRAD0_opsz24.svg" alt="Remover" class="icone-x">
 
                 </div>
             </div>
