982e3409aa353eb4d305481dd792b462d65247c5
seabruh <francisco.seabra@ua.pt>
2025-05-24 17:15:20 +0100

    seabraaa

--- NABU_base/Componentes/cp_perfil.php
+++ NABU_base/Componentes/cp_perfil.php
@@ -1,42 +1,50 @@
-<?php ?>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<div>
-  <aside>
-    <div class="container">
-      <div class="row">
-        <div class="col-sm-12 col-md-12 col-lg-12">
-          <img src="..\Imagens\image.png" alt="" class="img_user">
+<main class="body_index">
 
-      </div>
+    <div class="container py-4">
+        <div class="text-center mb-4">
+            <img src="../Imagens/produtos/ovos.jpg" alt="Foto de perfil" class="rounded-circle border border-success" width="100" height="100" style="object-fit: cover;">
+            <h2 class="mt-2 fs-5 text-success">Teresa Oliveira</h2>
+        </div>
 
+        <div class="card border-0 shadow-sm ">
+            <div class="list-group  list-group-flush">
+
+                <a href="#" class="list-group-item list-group-item-action d-flex align-items-center small verde_claro_bg">
+                    <img src="../Imagens/icons/add_circle_24dp_FFFFFF_FILL0_wght400_GRAD0_opsz24.svg" alt="Ícone" class="me-3" width="20" height="20">
+                    Os meus anúncios
+                </a>
+
+                <a href="#" class="list-group-item list-group-item-action d-flex align-items-center small verde_claro_bg">
+                    <img src="../Imagens/icons/add_circle_24dp_FFFFFF_FILL0_wght400_GRAD0_opsz24.svg" alt="Ícone" class="me-3" width="20" height="20">
+                    Log-in/ Sign up
+                </a>
+
+                <a href="#" class="list-group-item list-group-item-action d-flex align-items-center small verde_claro_bg">
+                    <img src="../Imagens/icons/add_circle_24dp_FFFFFF_FILL0_wght400_GRAD0_opsz24.svg" alt="Ícone" class="me-3" width="20" height="20">
+                    Dados pessoais
+                </a>
+
+                <a href="#" class="list-group-item list-group-item-action d-flex align-items-center small verde_claro_bg">
+                    <img src="../Imagens/icons/add_circle_24dp_FFFFFF_FILL0_wght400_GRAD0_opsz24.svg" alt="Ícone" class="me-3" width="20" height="20">
+                    Formas de Pagamento
+                </a>
+
+                <a href="#" class="list-group-item list-group-item-action d-flex align-items-center small verde_claro_bg">
+                    <img src="../Imagens/icons/add_circle_24dp_FFFFFF_FILL0_wght400_GRAD0_opsz24.svg" alt="Ícone" class="me-3" width="20" height="20">
+                    Definições
+                </a>
+
+                <a href="#" class="list-group-item list-group-item-action d-flex align-items-center small verde_claro_bg">
+                    <img src="../Imagens/icons/add_circle_24dp_FFFFFF_FILL0_wght400_GRAD0_opsz24.svg" alt="Ícone" class="me-3" width="20" height="20">
+                    Favoritos
+                </a>
+
+                <a href="#" class="list-group-item list-group-item-action d-flex align-items-center text-danger small verde_claro_bg">
+                    <img src="../Imagens/icons/add_circle_24dp_FFFFFF_FILL0_wght400_GRAD0_opsz24.svg" alt="Ícone" class="me-3" width="20" height="20">
+                    Logout
+                </a>
+            </div>
+        </div>
     </div>
 
-  </aside>
-</div>
-<div class="btn-group-vertical" role="group" aria-label="Vertical button group">
-
-  <button type="button" class="btn_nabu c_btn"><a href=".\cp_anunciosdetails.php"
-      class="a_link_nabu">Os meus Anúncios</a></button>
-
-  <button type="button" class="btn_nabu c_btn"><a href=".\cp_signup_login.php"
-      class="a_link_nabu">Login/Sign-up</a></button>
-
-  <button type="button" class="btn_nabu c_btn"><a href=".\cp_userdetails.php"
-      class="a_link_nabu">Dados Pessoais</a></button>
-
-  <button type="button" class="btn_nabu c_btn"><a href=".\cp_pagamentos_details.php"
-      class="a_link_nabu">Formas de Pagamento</a></button>
-
-       <button type="button" class="btn_nabu c_btn"><a href=".\cp_definicoes.php"
-      class="a_link_nabu">Definições</a></button>
-
-       <button type="button" class="btn_nabu c_btn"><a href=".\cp_favoritosdetails.php"
-      class="a_link_nabu">Foavoritos</a></button>
-       <button type="button" class="btn_nabu c_btn"><a href=".\cp_.php"
-      class="a_link_nabu"><span class="bs-danger-text-">Logout</span></a></button>
-</div>
+</main>
