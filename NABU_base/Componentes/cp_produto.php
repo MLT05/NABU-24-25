b5f3d1867585c8649d12fdb8a4846ab6fd1e8d3b
MLT05 <marianatavares18@ua.pt>
2025-05-26 12:32:37 +0100

    Botões na página produto

--- NABU_base/Componentes/cp_produto.php
+++ NABU_base/Componentes/cp_produto.php
@@ -37,10 +37,11 @@
     </div>
 
         <div class="botoes">
-            <button class="comprar" onclick="window.location.href='../Paginas/carrinho.php.html'">Comprar</button>
+            <button class="contactar" onclick="window.location.href='../Paginas/carrinho.php.html'">Carrinho</button>
             <button class="contactar" onclick="window.location.href='../Paginas/mensagens.php'">Contactar</button>
         </div>
-
-
+            <div class="botoes">
+                <button class="comprar" onclick="window.location.href='../Paginas/carrinho.php.html'">Comprar</button>
+            </div>
 </div>
 </main>
