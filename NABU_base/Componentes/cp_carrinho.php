03cfa3b678e0f74ebfb71a6b37fb4fd04b42ff73
MLT05 <marianatavares18@ua.pt>
2025-05-27 12:34:18 +0100

    carrinho, algumas coisas

--- NABU_base/Componentes/cp_carrinho.php
+++ NABU_base/Componentes/cp_carrinho.php
@@ -18,8 +18,8 @@ include_once ("cp_intro_carrinho.php");
                 </div>
                 <div class="col-7">
                     <div class="card-body py-2">
-                        <h2 class="verde fw-bold mb-1" >Tomates - Cacho</h2>
-                        <p class="card-text mb-0"><small class="text-muted">Rosa Silva</small></p>
+                        <h2 class="verde_escuro fw-bold mb-1" >Tomates - Cacho</h2>
+                        <p class="card-text verde mb-0"><small>Rosa Silva</small></p>
                     </div>
                 </div>
                 <div class="col-1 text-end pe-2">
@@ -29,43 +29,6 @@ include_once ("cp_intro_carrinho.php");
             </div>
         </div>
 
-        <!-- Card 2 -->
-        <div class="card mb-3 cards_homepage">
-            <div class="row g-0 align-items-center">
-                <div class="col-4">
-                    <img src="../Imagens/produtos/tomates.svg" class="img-fluid imagem_card_homepage rounded-start" alt="Tomates">
-                </div>
-                <div class="col-7">
-                    <div class="card-body py-2">
-                        <h2 class="verde fw-bold mb-1" >Tomates - Cacho</h2>
-                        <p class="card-text mb-0"><small class="text-muted">Rosa Silva</small></p>
-                    </div>
-                </div>
-                <div class="col-1 text-end pe-2">
-                    <img src="../Imagens/img_cp/close_24dp_004D40_FILL0_wght400_GRAD0_opsz24.svg" alt="Remover" class="icone-x">
-
-                </div>
-            </div>
-        </div>
-
-        <!-- Card 3 -->
-        <div class="card mb-3 cards_homepage">
-            <div class="row g-0 align-items-center">
-                <div class="col-4">
-                    <img src="../Imagens/produtos/tomates.svg" class="img-fluid imagem_card_homepage rounded-start" alt="Tomates">
-                </div>
-                <div class="col-7">
-                    <div class="card-body py-2">
-                        <h2 class="verde fw-bold mb-1" >Tomates - Cacho</h2>
-                        <p class="card-text mb-0"><small class="text-muted">Rosa Silva</small></p>
-                    </div>
-                </div>
-                <div class="col-1 text-end pe-2">
-                    <img src="../Imagens/img_cp/close_24dp_004D40_FILL0_wght400_GRAD0_opsz24.svg" alt="Remover" class="icone-x">
-
-                </div>
-            </div>
-        </div>
 
         <!-- Botão Finalizar -->
         <div class="top-buttons">
