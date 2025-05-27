ea06d633881cad7c2fa5509983eb52d36d06c90e
MLT05 <marianatavares18@ua.pt>
2025-05-27 12:27:23 +0100

    carrinho, algumas coisas

--- NABU_base/Componentes/cp_carrinho.php
+++ NABU_base/Componentes/cp_carrinho.php
@@ -18,46 +18,8 @@ include_once ("cp_intro_carrinho.php");
                 </div>
                 <div class="col-7">
                     <div class="card-body py-2">
-                        <h2 class="verde fw-bold mb-1" >Tomates - Cacho</h2>
-                        <p class="card-text mb-0"><small class="text-muted">Rosa Silva</small></p>
-                    </div>
-                </div>
-                <div class="col-1 text-end pe-2">
-                    <img src="../Imagens/img_cp/close_24dp_004D40_FILL0_wght400_GRAD0_opsz24.svg" alt="Remover" class="icone-x">
-
-                </div>
-            </div>
-        </div>
-
-        <!-- Card 2 -->
-        <div class="card mb-3 cards_homepage">
-            <div class="row g-0 align-items-center">
-                <div class="col-4">
-                    <img src="../Imagens/produtos/tomates.svg" class="img-fluid imagem_card_homepage rounded-start" alt="Tomates">
-                </div>
-                <div class="col-7">
-                    <div class="card-body py-2">
-                        <h2 class="verde fw-bold mb-1" >Tomates - Cacho</h2>
-                        <p class="card-text mb-0"><small class="text-muted">Rosa Silva</small></p>
-                    </div>
-                </div>
-                <div class="col-1 text-end pe-2">
-                    <img src="../Imagens/img_cp/close_24dp_004D40_FILL0_wght400_GRAD0_opsz24.svg" alt="Remover" class="icone-x">
-
-                </div>
-            </div>
-        </div>
-
-        <!-- Card 3 -->
-        <div class="card mb-3 cards_homepage">
-            <div class="row g-0 align-items-center">
-                <div class="col-4">
-                    <img src="../Imagens/produtos/tomates.svg" class="img-fluid imagem_card_homepage rounded-start" alt="Tomates">
-                </div>
-                <div class="col-7">
-                    <div class="card-body py-2">
-                        <h2 class="verde fw-bold mb-1" >Tomates - Cacho</h2>
-                        <p class="card-text mb-0"><small class="text-muted">Rosa Silva</small></p>
+                        <h2 class="verde_escuro fw-bold mb-1" >Tomates - Cacho</h2>
+                        <p class="verde mb-0"><small >Rosa Silva</small></p>
                     </div>
                 </div>
                 <div class="col-1 text-end pe-2">
@@ -69,7 +31,7 @@ include_once ("cp_intro_carrinho.php");
 
         <!-- Botão Finalizar -->
         <div class="top-buttons">
-            <button type="button" class="btn botao_carrinho" data-bs-toggle="modal" data-bs-target="#pedidoModal">
+            <button type="button" class=" verde_escuro btn botao_carrinho" data-bs-toggle="modal" data-bs-target="#pedidoModal">
                 Finalizar Pedido
             </button>
         </div>
