5e1b517344b12ee8cf76c02c1ad96eafbd0280d2
MariaLeonorFrias <marialeonorfrias@ua.pt>
2025-06-19 18:27:44 +0100

    Merge remote-tracking branch 'origin/marileide' into marileide

--- NABU_base/Componentes/cp_pesquisa.php
+++ NABU_base/Componentes/cp_pesquisa.php
@@ -23,8 +23,7 @@ $link = new_db_connection();
         <?php
         // Código PHP para ir buscar os produtos à base de dados
         $stmt = mysqli_stmt_init($link);
-        $query = "SELECT id_anuncio, nome_produto, preco, abreviatura , capa FROM anuncios INNER JOIN medidas ON ref_medida = id_medida WHERE ref_user = ?";
-        $capa = "default-image.jpg"; // imagem padrão caso não tenha capa
+        $query = "SELECT id_anuncio, nome_produto, preco, abreviatura , capa FROM anuncios INNER JOIN medidas ON anuncios.ref_medida = medidas.id_medida FROM anuncios ";
 
         if (mysqli_stmt_prepare($stmt, $query)) {
             mysqli_stmt_execute($stmt);
@@ -33,47 +32,48 @@ $link = new_db_connection();
             while (mysqli_stmt_fetch($stmt)) {
                 ?>
                 <div class="col-6">
-                    <a href="../Paginas/produto.php?id=<?= $id_anuncio ?>" style="text-decoration: none">
-                        <div class="card rounded-4 shadow-sm border-0 position-relative card_pesquisa">
+                    <div class="col-6">
+                        <a href="../Paginas/produto.php?id=<?= $id_anuncio ?>" style="text-decoration: none">
+                            <div class="card rounded-4 shadow-sm border-0 position-relative card_pesquisa">
 
-                            <!-- Ícone de favorito no canto superior direito -->
-                            <div class="position-absolute top-0 end-0 m-2 d-flex justify-content-center align-items-center rounded-circle shadow favorite-circle">
+                                <!-- Ícone de favorito no canto superior direito -->
+                                <div class="position-absolute top-0 end-0 m-2 d-flex justify-content-center align-items-center rounded-circle shadow favorite-circle">
                     <span class="material-symbols-outlined verde_escuro">
                         favorite
                     </span>
-                            </div>
-
-                            <!-- Imagem -->
-                            <div class="imagem_card_pesquisa">
-                                <img src="../uploads/capas/default-image.jpg" class="card-img-top rounded-4 img_hp_card" alt="Tomates">
-                            </div>
-
-                            <!-- Conteúdo -->
-                            <div class="card-body m-2 pt-2 px-2 pb-0">
-                                <h6 class="card-title mb-1 fw-semibold verde_escuro align-middle fs-3 text-truncate"><?php echo htmlspecialchars($nome_produto); ?></h6>
-                            </div>
-
-                            <hr class="linha-card verde_escuro">
-
-
-
-                            <div class="card-body m-2 pt-0 pb-2 px-2">
-                                <div class="d-flex justify-content-between align-items-center">
-                                    <form action="../Paginas/editar_produto.php" method="POST" class="d-inline">
-                                        <input type="hidden" name="id" value="<?php echo htmlspecialchars($id_anuncio); ?>">
-
-                                    </form>
-                                    <small class="verde_escuro fw-bolder fs-5">
-                                        <i class="bi bi-star-fill"></i>
-                                    </small>
-                                    <small class="fw-bolder verde_escuro fs-5"><?php echo htmlspecialchars($preco); ?> € /<?php echo htmlspecialchars($medida); ?></small>
                                 </div>
+
+                                <!-- Imagem -->
+                                <div class="imagem_card_pesquisa">
+                                    <img src="../uploads/capas/default-image.jpg" class="card-img-top rounded-4 img_hp_card" alt="Tomates">
+                                </div>
+
+                                <!-- Conteúdo -->
+                                <div class="card-body m-2 pt-2 px-2 pb-0">
+                                    <h6 class="card-title mb-1 fw-semibold verde_escuro align-middle fs-3 text-truncate"><?php echo htmlspecialchars($nome_produto); ?></h6>
+                                </div>
+
+                                <hr class="linha-card verde_escuro">
+
+
+
+                                <div class="card-body m-2 pt-0 pb-2 px-2">
+                                    <div class="d-flex justify-content-between align-items-center">
+                                        <form action="../Paginas/editar_produto.php" method="POST" class="d-inline">
+                                            <input type="hidden" name="id" value="<?php echo htmlspecialchars($id_anuncio); ?>">
+
+                                        </form>
+                                        <small class="verde_escuro fw-bolder fs-5">
+                                            <i class="bi bi-star-fill"></i>
+                                        </small>
+                                        <small class="fw-bolder verde_escuro fs-5"><?php echo htmlspecialchars($preco); ?> € /<?php echo htmlspecialchars($medida); ?></small>
+                                    </div>
+                                </div>
+
+
                             </div>
-
-
-                        </div>
-                    </a>
-                </div>
+                        </a>
+                    </div>
                 <?php
             }
 
