45bf4530c3b5cedb63061d760a7ff1de818553d3
MariaLeonorFrias <marialeonorfrias@ua.pt>
2025-06-19 19:31:47 +0100

    Merge remote-tracking branch 'origin/marileide' into marileide

--- NABU_base/Componentes/cp_index.php
+++ NABU_base/Componentes/cp_index.php
@@ -1,162 +1,19 @@
 <?php include_once("cp_intro_index.php"); ?>
 <main class="body_index">
 
-    <!-- Botões topo -->
-    <div class="top-buttons mb-4 d-flex gap-2">
-        <a class="responsive-button active">Para si</a>
-        <a href="../Paginas/mapa.php" class="responsive-button">Mapa</a>
-    </div>
-
     <!-- RECOMENDAÇÕES -->
     <section class="mb-5">
         <h1 class="verde_escuro">Recomendações</h1>
-        <div id="carouselrecomendacoes" class="carousel slide" data-bs-ride="carousel">
-            <div class="carousel-inner">
-                <!-- Item 1 -->
-                <div class="carousel-item active">
-                    <a href="../Paginas/produto.php" class="text-decoration-none text-dark">
-                        <div class="cards_homepage card-body rounded-4 shadow-sm bg-light-green">
-                            <div class="imagem_card_homepage">
-                                <img class="img_hp_card" src="../Imagens/produtos/tomates.svg" alt="Tomates">
-                            </div>
-                            <div class="p-3">
-                                <h2 class="verde_escuro fw-bold mb-1">Tomates / Cacho</h2>
-                                <p class="text-muted mb-2">Rosa - Quinta da Fonte</p>
-                                <hr>
-                                <div class="d-flex justify-content-end">
-                                    <h3 class="verde_escuro fw-semibold mb-0">2€</h3>
-                                </div>
-                            </div>
-                        </div>
-                    </a>
-                </div>
-
-                <!-- Item 2 -->
-                <div class="carousel-item">
-                    <a href="../Paginas/produto.php" class="text-decoration-none text-dark">
-                        <div class="cards_homepage card-body rounded-4 shadow-sm bg-light-green">
-                            <div class="imagem_card_homepage">
-                                <img class="img_hp_card" src="../Imagens/produtos/alface.jpg" alt="Alface">
-                            </div>
-                            <div class="p-3">
-                                <h2 class="verde_escuro fw-bold mb-1">Alface/kg</h2>
-                                <p class="text-muted mb-2">Alfredo Rocha - Quinta Coelho</p>
-                                <hr>
-                                <div class="d-flex justify-content-end">
-                                    <h3 class="verde_escuro fw-semibold mb-0">2,49 €</h3>
-                                </div>
-                            </div>
-                        </div>
-                    </a>
-                </div>
-
-                <!-- Item 3 -->
-                <div class="carousel-item">
-                    <a href="../Paginas/produto.php" class="text-decoration-none text-dark">
-                        <div class="cards_homepage card-body rounded-4 shadow-sm bg-light-green">
-                            <div class="imagem_card_homepage">
-                                <img class="img_hp_card" src="../Imagens/produtos/ovos.jpg" alt="Ovos">
-                            </div>
-                            <div class="p-3">
-                                <h2 class="verde_escuro fw-bold mb-1">Ovos / unidade</h2>
-                                <p class="text-muted mb-2">Quim Roscas - Rua do Zeca Estacionamento Nr84</p>
-                                <hr>
-                                <div class="d-flex justify-content-end">
-                                    <h3 class="verde_escuro fw-semibold mb-0">0,20 €</h3>
-                                </div>
-                            </div>
-                        </div>
-                    </a>
-                </div>
-            </div>
-
-            <!-- Controles -->
-            <button class="carousel-control-prev" type="button" data-bs-target="#carouselrecomendacoes" data-bs-slide="prev">
-                <span class="carousel-control-prev-icon"></span>
-                <span class="visually-hidden">Anterior</span>
-            </button>
-            <button class="carousel-control-next" type="button" data-bs-target="#carouselrecomendacoes" data-bs-slide="next">
-                <span class="carousel-control-next-icon"></span>
-                <span class="visually-hidden">Seguinte</span>
-            </button>
-        </div>
+        <?php include_once("cp_recomendacoes.php"); ?>
     </section>
 
+    <!-- NOVIDADES -->
     <section class="mb-5">
         <h1 class="verde_escuro">Novidades</h1>
-        <div id="carouselnovidades" class="carousel slide" >
-            <div class="carousel-inner">
-                <!-- Item 1 -->
-                <div class="carousel-item active">
-                    <a href="../Paginas/produto.php" class="text-decoration-none text-dark">
-                        <div class="cards_homepage card-body rounded-4 shadow-sm bg-light-green">
-                            <div class="imagem_card_homepage">
-                                <img class="img_hp_card" src="../Imagens/produtos/tomates.svg" alt="Tomates">
-                            </div>
-                            <div class="p-3">
-                                <h2 class="verde_escuro fw-bold mb-1">Tomates / Cacho</h2>
-                                <p class="text-muted mb-2">Rosa - Quinta da Fonte</p>
-                                <hr>
-                                <div class="d-flex justify-content-end">
-                                    <h3 class="verde_escuro fw-semibold mb-0">2€</h3>
-                                </div>
-                            </div>
-                        </div>
-                    </a>
-                </div>
-
-                <!-- Item 2 -->
-                <div class="carousel-item">
-                    <a href="../Paginas/produto.php" class="text-decoration-none text-dark">
-                        <div class="cards_homepage card-body rounded-4 shadow-sm bg-light-green">
-                            <div class="imagem_card_homepage">
-                                <img class="img_hp_card" src="../Imagens/produtos/alface.jpg" alt="Alface">
-                            </div>
-                            <div class="p-3">
-                                <h2 class="verde_escuro fw-bold mb-1">Alface/kg</h2>
-                                <p class="text-muted mb-2">Alfredo Rocha - Quinta Coelho</p>
-                                <hr>
-                                <div class="d-flex justify-content-end">
-                                    <h3 class="verde_escuro fw-semibold mb-0">2,49 €</h3>
-                                </div>
-                            </div>
-                        </div>
-                    </a>
-                </div>
-
-                <!-- Item 3 -->
-                <div class="carousel-item">
-                    <a href="../Paginas/produto.php" class="text-decoration-none text-dark">
-                        <div class="cards_homepage card-body rounded-4 shadow-sm bg-light-green">
-                            <div class="imagem_card_homepage">
-                                <img class="img_hp_card" src="../Imagens/produtos/ovos.jpg" alt="Ovos">
-                            </div>
-                            <div class="p-3">
-                                <h2 class="verde_escuro fw-bold mb-1">Ovos / unidade</h2>
-                                <p class="text-muted mb-2">Quim Roscas - Rua do Zeca Estacionamento Nr84</p>
-                                <hr>
-                                <div class="d-flex justify-content-end">
-                                    <h3 class="verde_escuro fw-semibold mb-0">0,20 €</h3>
-                                </div>
-                            </div>
-                        </div>
-                    </a>
-                </div>
-            </div>
-
-            <!-- Controles -->
-            <button class="carousel-control-prev" type="button" data-bs-target="#carouselnovidades" data-bs-slide="prev">
-                <span class="carousel-control-prev-icon"></span>
-                <span class="visually-hidden">Anterior</span>
-            </button>
-            <button class="carousel-control-next" type="button" data-bs-target="#carouselnovidades" data-bs-slide="next">
-                <span class="carousel-control-next-icon"></span>
-                <span class="visually-hidden">Seguinte</span>
-            </button>
-        </div>
+        <?php include_once("cp_novidades.php"); ?>
     </section>
 
-    <!-- Favoritos -->
+    <!-- FAVORITOS -->
     <section class="mb-5">
         <h1 class="verde_escuro">Favoritos</h1>
         <div class="row g-3">
