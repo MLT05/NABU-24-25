6dc5aad401056e3f97a3ccf283aa4e83eaf16560
MariaLeonorFrias <marialeonorfrias@ua.pt>
2025-06-19 18:17:03 +0100

    Merge remote-tracking branch 'origin/marileide' into marileide

--- NABU_base/Componentes/cp_pesquisa.php
+++ NABU_base/Componentes/cp_pesquisa.php
@@ -23,11 +23,12 @@ $link = new_db_connection();
         <?php
         // Código PHP para ir buscar os produtos à base de dados
         $stmt = mysqli_stmt_init($link);
-        $query = "SELECT anuncios.nome_produto, anuncios.preco, anuncios.id_anuncio FROM anuncios ";
+        $query = "SELECT id_anuncio, nome_produto, preco, abreviatura , capa FROM anuncios INNER JOIN medidas ON ref_medida = id_medida WHERE ref_user = ?";
+        $capa = "default-image.jpg"; // imagem padrão caso não tenha capa
 
         if (mysqli_stmt_prepare($stmt, $query)) {
             mysqli_stmt_execute($stmt);
-            mysqli_stmt_bind_result($stmt, $nome, $preco, $id_anuncio);
+            mysqli_stmt_bind_result($stmt, $id_anuncio, $nome_produto, $preco, $medida, $capa);
 
             while (mysqli_stmt_fetch($stmt)) {
                 ?>
@@ -44,24 +45,32 @@ $link = new_db_connection();
 
                             <!-- Imagem -->
                             <div class="imagem_card_pesquisa">
-                                <img src="../Imagens/produtos/<?php echo $id_anuncio; ?>" class="card-img-top rounded-4 img_hp_card" alt="<?php echo $nome; ?>">
+                                <img src="../uploads/capas/default-image.jpg" class="card-img-top rounded-4 img_hp_card" alt="Tomates">
                             </div>
 
                             <!-- Conteúdo -->
                             <div class="card-body m-2 pt-2 px-2 pb-0">
-                                <h6 class="card-title mb-1 fw-semibold verde_escuro align-middle fs-3"><?php echo $nome; ?></h6>
+                                <h6 class="card-title mb-1 fw-semibold verde_escuro align-middle fs-3 text-truncate"><?php echo htmlspecialchars($nome_produto); ?></h6>
                             </div>
 
                             <hr class="linha-card verde_escuro">
 
+
+
                             <div class="card-body m-2 pt-0 pb-2 px-2">
                                 <div class="d-flex justify-content-between align-items-center">
+                                    <form action="../Paginas/editar_produto.php" method="POST" class="d-inline">
+                                        <input type="hidden" name="id" value="<?php echo htmlspecialchars($id_anuncio); ?>">
+
+                                    </form>
                                     <small class="verde_escuro fw-bolder fs-5">
                                         <i class="bi bi-star-fill"></i>
                                     </small>
-                                    <small class="fw-bolder verde_escuro fs-5"><?php echo number_format($preco, 2); ?> €</small>
+                                    <small class="fw-bolder verde_escuro fs-5"><?php echo htmlspecialchars($preco); ?> € /<?php echo htmlspecialchars($medida); ?></small>
                                 </div>
                             </div>
+
+
                         </div>
                     </a>
                 </div>
