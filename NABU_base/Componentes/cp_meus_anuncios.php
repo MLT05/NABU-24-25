8e39e53d5fdc53cc728f8d290712e52f0d61e4c4
MariaLeonorFrias <marialeonorfrias@ua.pt>
2025-06-19 18:46:59 +0100

    Merge remote-tracking branch 'origin/marileide' into marileide

--- NABU_base/Componentes/cp_meus_anuncios.php
+++ NABU_base/Componentes/cp_meus_anuncios.php
@@ -1,90 +1,76 @@
+<?php
+// Conexão à base de dados
+require_once '../Connections/connection.php';
+$link = new_db_connection();
+?>
 
 <main class="body_index">
-<?php
+    <div class="mt-3">
 
-require_once '../Connections/connection.php';
+        <!-- 🔍 Pesquisa -->
+        <?php
+        require_once '../Componentes/cp_intro_pesquisa.php';
+        require_once '../Componentes/cp_intro_categorias.php';
+        ?>
 
-if (!isset($_SESSION['id_user'])) {
-    // Se não estiver logado, redireciona pro login
-    header("../Paginas/login.php");
-
-} else {
-
-
-    $id_user = $_SESSION['id_user'];
-
-    $link = new_db_connection();
-    $stmt = mysqli_stmt_init($link);
-
-    $query = "SELECT id_anuncio, nome_produto, preco, abreviatura , capa FROM anuncios INNER JOIN medidas ON ref_medida = id_medida WHERE ref_user = ?";
-
-$capa = "default-image.jpg"; // imagem padrão caso não tenha capa
-?>
-    <section class="mb-5">
-        <h1 class="verde_escuro">Meus anúncios:</h1>
+        <!-- 🧺 Produtos -->
         <div class="row g-3">
-
-<?php
-
-
-    if (mysqli_stmt_prepare($stmt, $query)) {
-        mysqli_stmt_bind_param($stmt, 'i', $id_user);
-        mysqli_stmt_execute($stmt);
-        mysqli_stmt_bind_result($stmt, $id_anuncio, $nome_produto, $preco, $medida, $capa);
-
-        while(mysqli_stmt_fetch($stmt)) { ?>
-
-            <div class="col-6">
-
-                    <div class="card rounded-4 shadow-sm border-0 position-relative card_pesquisa">
-
-
-
-                        <a href="../paginas/produto.php?id=<?php echo htmlspecialchars($id_anuncio); ?>" style="text-decoration: none">
-                        <!-- Imagem -->
-                        <div class="imagem_card_pesquisa">
-                            <img src="../uploads/capas/default-image.jpg" class="card-img-top rounded-4 img_hp_card" alt="Tomates">
-                        </div>
-
-                        <!-- Conteúdo -->
-                        <div class="card-body m-2 pt-2 px-2 pb-0">
-                            <h6 class="card-title mb-1 fw-semibold verde_escuro align-middle fs-3 text-truncate"><?php echo htmlspecialchars($nome_produto); ?></h6>
-                        </div>
-                        </a>
-
-                        <hr class="linha-card verde_escuro">
-
-                        <div class="card-body m-2 pt-0 pb-2 px-2">
-                            <div class="d-flex justify-content-between align-items-center">
-                                <form action="../Paginas/editar_produto.php" method="POST" class="d-inline">
-                                    <input type="hidden" name="id" value="<?php echo htmlspecialchars($id_anuncio); ?>">
-                                    <button type="submit" class="btn verde_escuro_bg rounded-circle d-flex align-items-center justify-content-center p-0" style="width: 40px; height: 40px;">
-                                        <span class="material-icons text-white">edit</span>
-                                    </button>
-                                </form>
-                                <small class="fw-bolder verde_escuro fs-5"><?php echo htmlspecialchars($preco); ?> € /<?php echo htmlspecialchars($medida); ?></small>
-                            </div>
-                        </div>
-
-            </div>
-        </div>
-
             <?php
-        }
+            $stmt = mysqli_stmt_init($link);
+            $query = "SELECT id_anuncio, nome_produto, preco, abreviatura , capa
+                      FROM anuncios
+                      INNER JOIN medidas ON anuncios.ref_medida = medidas.id_medida";
 
+            if (mysqli_stmt_prepare($stmt, $query)) {
+                mysqli_stmt_execute($stmt);
+                mysqli_stmt_bind_result($stmt, $id_anuncio, $nome_produto, $preco, $medida, $capa);
 
+                while (mysqli_stmt_fetch($stmt)) {
+                    ?>
+                    <div class="col-6">
+                        <div class="card rounded-4 shadow-sm border-0 position-relative card_pesquisa">
 
-        mysqli_stmt_close($stmt);
-    }
+                            <!-- Ícone de favorito -->
+                            <div class="position-absolute top-0 end-0 m-2 d-flex justify-content-center align-items-center rounded-circle shadow favorite-circle">
+                                <span class="material-symbols-outlined verde_escuro">favorite</span>
+                            </div>
 
+                            <!-- Link do produto -->
+                            <a href="../Paginas/produto.php?id=<?= $id_anuncio ?>" style="text-decoration: none">
+                                <!-- Imagem -->
+                                <div class="imagem_card_pesquisa">
+                                    <img src="../uploads/capas/default-image.jpg" class="card-img-top rounded-4 img_hp_card" alt="<?= htmlspecialchars($nome_produto) ?>">
+                                </div>
 
-    mysqli_close($link);
-}
-?>
+                                <!-- Título -->
+                                <div class="card-body m-2 pt-2 px-2 pb-0">
+                                    <h6 class="card-title mb-1 fw-semibold verde_escuro align-middle fs-3 text-truncate">
+                                        <?= htmlspecialchars($nome_produto) ?>
+                                    </h6>
+                                </div>
+                            </a>
 
+                            <hr class="linha-card verde_escuro">
 
+                            <!-- Rodapé -->
+                            <div class="card-body m-2 pt-0 pb-2 px-2">
+                                <div class="d-flex justify-content-between align-items-center">
+                                    <small class="verde_escuro fw-bolder fs-5">
+                                        <i class="bi bi-star-fill"></i>
+                                    </small>
+                                    <small class="fw-bolder verde_escuro fs-5">
+                                        <?= number_format($preco, 2) ?> € / <?= htmlspecialchars($medida) ?>
+                                    </small>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                    <?php
+                }
+                mysqli_stmt_close($stmt);
+            }
+            ?>
         </div>
-
-    </section>
-
+    </div>
 </main>
