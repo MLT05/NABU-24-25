f3e3d957b5d2b9819b9291be0cdf81daac75d520
Lucas Sérgio <noirlove13@gmail.com>
2025-06-20 09:21:36 +0100

    Bases do pagamento

---
+++ NABU_base/Componentes/cp_pagamentos_details.php
@@ -0,0 +1,36 @@
+<main class="body_index">
+
+    <div class="container py-2">
+        <div class="text-center mb-4"></div>
+        <h2 class="mt-2 verde_escuro">Detalhes de pagamento</h2>
+    </div>
+
+    <div class="card border-0 shadow-sm ">
+        <div class="list-group list-group-flush">
+
+            <?php
+            echo '
+
+            <a href="../Paginas/perfil_details.php"
+                class="verde_escuro list-group-item list-group-item-action d-flex align-items-center verde_claro_bg">
+
+                Adicione metéodos de pagamento
+            </a>
+
+            <a href=".\cp_pagamentos_details.php"
+                class="verde_escuro list-group-item list-group-item-action d-flex align-items-center verde_claro_bg">
+
+                Histórico de Pagamentos
+            </a>
+
+            <a href="#"
+                class="verde_escuro list-group-item list-group-item-action d-flex align-items-center verde_claro_bg">
+
+                Apagar metéodos de pagamento
+            </a>
+
+        </div>';
+
+
+            ?>
+</main>
