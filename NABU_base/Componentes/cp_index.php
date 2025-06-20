9df54c5bbb1b3389f99a5ddf12c07396725c4b4e
MariaLeonorFrias <marialeonorfrias@ua.pt>
2025-06-20 15:13:42 +0100

    Merge remote-tracking branch 'origin/marileide' into marileide

--- NABU_base/Componentes/cp_index.php
+++ NABU_base/Componentes/cp_index.php
@@ -3,11 +3,7 @@
 <main class="body_index">
 
 
-    <!-- Botões topo -->
-    <div class="top-buttons mb-4 d-flex gap-2">
-        <a class="responsive-button active">Para si</a>
-        <a href="../Paginas/mapa.php" class="responsive-button">Mapa</a>
-    </div>
+    <?php require_once "cp_intro_index.php" ?>
 
 
 
