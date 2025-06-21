49b3e2a21859778ed6855ccd68b35447bef97ec9
MariaLeonorFrias <marialeonorfrias@ua.pt>
2025-06-21 16:07:32 +0100

    Merge remote-tracking branch 'origin/marileide' into marileide

--- NABU_base/Componentes/cp_mapa.php
+++ NABU_base/Componentes/cp_mapa.php
@@ -21,7 +21,7 @@
         const map = new mapboxgl.Map({
             container: 'map', // container ID
             center: [lng, lat], // starting position [lng, lat]. Note that lat must be set between -90 and 90
-            zoom: 10 // starting zoom
+            zoom: 7 // starting zoom
         });
 
 
