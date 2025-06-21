62a4fd0f2c7c3d0d575a1dd4de8e898f4ede3f66
MariaLeonorFrias <marialeonorfrias@ua.pt>
2025-06-21 16:01:45 +0100

    Merge remote-tracking branch 'origin/marileide' into marileide

--- NABU_base/Componentes/cp_mapa.php
+++ NABU_base/Componentes/cp_mapa.php
@@ -38,7 +38,7 @@
 
                 const popupHTML = `
   <div style="min-width: 220px; font-family: sans-serif; font-size: 14px; color: #333;">
-    <h3 style="margin: 0 0 5px 0; font-size: 16px; color: #2c3e50;">${anuncio.nome_produto}</h3>
+   <h3 style="margin: 0 0 5px 0;">${anuncio.nome_produto}</h3>
     <p style="margin: 0;">📍 ${anuncio.localizacao}</p>
     <p style="margin: 5px 0;"><strong style="color: #27ae60;">${anuncio.preco} € / ${anuncio.ref_medida}</strong></p>
     <a href="./produto.php?id=${anuncio.id}" target="_blank" style="color: #2980b9; text-decoration: underline;">Ver mais</a>
