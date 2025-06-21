fc233da0770fda99235e8454964fcb3346e775ea
CarlT09 <carlosteixeira05@ua.pt>
2025-06-21 15:52:21 +0100

    html cp_mensagensdetails.php feito

--- NABU_base/Paginas/mensagens_details.php
+++ NABU_base/Paginas/mensagens_details.php
@@ -1,4 +1,4 @@
-<?php $bodyClass = ''; ?>
+<?php $bodyClass = 'min-vh-100 d-flex flex-column'; ?>
 <?php include_once "../Componentes/cp_head.php" ?>
 
 <?php include_once "../Componentes/cp_header.php" ?>
